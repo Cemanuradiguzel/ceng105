4b744af548f2ab5bfda8136a51272d9498f51da8
CaglaPelin <118660952+CaglaPelin@users.noreply.github.com>
2022-11-20 19:17:13 +0300

    Update ceng105.c

--- ceng105.c
+++ ceng105.c
@@ -6,7 +6,9 @@ int main(void) {
   printf("Vize notlarınızı giriniz:");
    scanf("%d %d", &vize1, &vize2);
   printf("Final notunuzu giriniz:");
-  
+  scanf("%d",&final);
+  ort=vize1*0.3+vize2*0.3+final*0.4;
+   
   
   
   
