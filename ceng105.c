547b06b5c2a3a30c7c4268444303fd250acf8826
SezinKorogluu <118663626+SezinKorogluu@users.noreply.github.com>
2022-11-20 19:13:26 +0300

    giving input

--- ceng105.c
+++ ceng105.c
@@ -3,7 +3,9 @@ int main(void) {
    
   int vize1, vize2, final;
   float ort;
-  
+  printf("Vize notlarınızı giriniz:");
+   scanf("%d %d", &vize1, &vize2);
+  printf("Final notunuzu giriniz:");
   
   
   
