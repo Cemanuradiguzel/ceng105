5a441f514403c7d25cb16d1aaf20a99cfdb8fe88
Cemanuradiguzel <94220168+Cemanuradiguzel@users.noreply.github.com>
2022-11-20 19:22:11 +0300

    Update ceng105.c

--- ceng105.c
+++ ceng105.c
@@ -2,14 +2,19 @@
 int main(void) {
    
   int vize1, vize2, final;
+  char grade;
   float ort;
-  printf("Vize notlarınızı giriniz:");
+   printf("Vize notlarınızı giriniz:");
    scanf("%d %d", &vize1, &vize2);
-  printf("Final notunuzu giriniz:");
-  scanf("%d",&final);
-  ort=vize1*0.3+vize2*0.3+final*0.4;
+   printf("Final notunuzu giriniz:");
+   scanf("%d",&final);
+   ort=vize1*0.3+vize2*0.3+final*0.4;
    
-  
+   if(ort=>90)
+      grade = 'A';
+   else if(ort=>80)
+      grade = 'B';
+   
   
   
   
